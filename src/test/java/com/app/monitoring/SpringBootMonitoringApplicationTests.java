c3b6d52d5a9e159385f6aed4d8fcc74a80be4d9f
jogendra-patro1 <jkumar.p123@gmail.com>
2023-07-13 22:59:46 +0530

    My First commit

---
+++ src/test/java/com/app/monitoring/SpringBootMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMonitoringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
